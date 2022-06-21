6db46fdb78c39306cdec9f4ededec7a00191dcd7
laurargs <103062837+laurargs@users.noreply.github.com>
2022-06-21 13:10:25 -0300

    Create Servidor.java

---
+++ Servidor.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class Servidor {
+   
+    public static final int TAMANHO_BACKLOG = 100;
+
+    public static void main(String[] args) throws IOException {
+       
+        InetSocketAddress bindAddr = new InetSocketAddress(46317);
+        HttpServer server = HttpServer.create(bindAddr, TAMANHO_BACKLOG);
+        server.setExecutor(Executors.newSingleThreadExecutor());
+
+        server.createContext(IndexHandler.PATH, new IndexHandler());
+   
+        server.start();
+        System.out.printf("Servidor ouvindo requisições na porta %s\n\n", server.getAddress().getPort());
+    }
+}
