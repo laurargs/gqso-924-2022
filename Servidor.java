2f708e172d5197923d4fb3397a2537d9e4c6ffa7
laurargs <103062837+laurargs@users.noreply.github.com>
2022-06-21 13:43:37 -0300

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -27,30 +27,3 @@ public class Servidor {
 }
 
 
-public class IndexHandler implements HttpHandler {
-
-    public static final String PATH = "/";
-
-    @Override
-    public void handle(HttpExchange conn) throws IOException {
-        byte[] mensagem = "Seja bem-vindo(a) a API Calculadora! Temos o seguinte endpoint: /div".getBytes();
-
-        try {
-            
-            conn.sendResponseHeaders(HTTP_OK, mensagem.length);
-
-            Headers headers = conn.getResponseHeaders();
-            headers.add("Content-Type", "text/html; charset=UTF-8");
-            
-            try (OutputStream out = conn.getResponseBody()) {
-                out.write(mensagem);
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            conn.close();
-        }
-    }
-}
