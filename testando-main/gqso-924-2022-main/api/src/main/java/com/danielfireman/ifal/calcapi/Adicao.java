4ed2d902b3a602fbf73b29ad25dbe64c6424d5fe
Jocineidebb <jbbs2@aluno.ifal.edu.br>
2022-07-26 15:45:06 +0000

    Alteracao

---
+++ testando-main/gqso-924-2022-main/api/src/main/java/com/danielfireman/ifal/calcapi/Adicao.java
@@ -0,0 +1,22 @@
+package com.danielfireman.ifal.calcapi;
+
+import io.jooby.annotations.GET;
+import io.jooby.annotations.Path;
+import io.jooby.annotations.PathParam;
+import io.jooby.exception.BadRequestException;
+
+@Path("/soma/{p1}/{p2}")
+public class Adicao {
+
+    @GET
+    public double calculaRaiz(@PathParam("p1") Double p1, @PathParam("p2") Double p2) {
+
+        try {
+            double soma = p1/p2;
+            return soma;
+        } catch (NumberFormatException nfe) {
+            throw new BadRequestException("Parâmetro inválido: " +p1+p2);
+        }
+    }
+    
+}
