d65807857f1dcc6485a89d39c5f32f7e75fa87a6
Jocineidebb <jbbs2@aluno.ifal.edu.br>
2022-07-26 15:58:23 +0000

    Mudanca

--- testando-main/gqso-924-2022-main/api/src/main/java/com/danielfireman/ifal/calcapi/Adicao.java
+++ testando-main/gqso-924-2022-main/api/src/main/java/com/danielfireman/ifal/calcapi/Adicao.java
@@ -12,7 +12,7 @@ public class Adicao {
     public double calculaRaiz(@PathParam("p1") Double p1, @PathParam("p2") Double p2) {
 
         try {
-            double soma = p1/p2;
+            double soma = p1+p2;
             return soma;
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Parâmetro inválido: " +p1+p2);
--- testando-main/gqso-924-2022-main/api/src/test/java/com/danielfireman/ifal/calcapi/AdicaoTest.java
+++ testando-main/gqso-924-2022-main/api/src/test/java/com/danielfireman/ifal/calcapi/AdicaoTest.java
@@ -12,35 +12,35 @@ import io.jooby.exception.BadRequestException;
 public class DivisaoTest {
 
     @Test
-    public void Divisao1() {
+    public void Adicao1() {
         MockRouter router = new MockRouter(new App());
-        router.get("/div/4/2", rsp -> {
-            assertEquals(2.0, rsp.value());
+        router.get("/soma/4/2", rsp -> {
+            assertEquals(6.0, rsp.value());
             assertEquals(StatusCode.OK, rsp.getStatusCode());
         });
     }
 
     @Test
-    public void Divisao2() {
+    public void Adicao2() {
         MockRouter router = new MockRouter(new App());
-        router.get("/div/16/4", rsp -> {
-            assertEquals(4.0, rsp.value());
+        router.get("/soma/16/4", rsp -> {
+            assertEquals(20, rsp.value());
             assertEquals(StatusCode.OK, rsp.getStatusCode());
         });
     }
 
     @Test
-    public void Divisao3() {
+    public void Adicao3() {
         MockRouter router = new MockRouter(new App());
         assertThrows(BadRequestException.class, () ->{
-            router.get("/div/aa/2", rsp -> {});
+            router.get("/soma/aa/2", rsp -> {});
         });
     }
     @Test
-    public void Divisao4() {
+    public void Adicao4() {
         MockRouter router = new MockRouter(new App());
         assertThrows(BadRequestException.class, () ->{
-            router.get("/div/8,5/2", rsp -> {});
+            router.get("/soma/8,5/2", rsp -> {});
         });
     }
 
