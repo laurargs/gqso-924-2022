29c58af91f061b66077dbd1ad32f95778d940951
laurargs <lrgs1@aluno.ifal.edu.br>
2022-07-26 17:30:08 +0000

    mudanca

--- testando-main/gqso-924-2022-main/api/src/main/java/com/danielfireman/ifal/calcapi/Subtracao.java
+++ testando-main/gqso-924-2022-main/api/src/main/java/com/danielfireman/ifal/calcapi/Subtracao.java
@@ -5,15 +5,15 @@ import io.jooby.annotations.Path;
 import io.jooby.annotations.PathParam;
 import io.jooby.exception.BadRequestException;
 
-@Path("/soma/{p1}/{p2}")
-public class Adicao {
+@Path("/sub/{p1}/{p2}")
+public class Subtracao {
 
     @GET
     public double calculaRaiz(@PathParam("p1") Double p1, @PathParam("p2") Double p2) {
 
         try {
-            double soma = p1+p2;
-            return soma;
+            double sub = p1-p2;
+            return sub;
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Parâmetro inválido: " +p1+p2);
         }
